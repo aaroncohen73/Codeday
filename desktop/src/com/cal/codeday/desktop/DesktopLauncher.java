3aa963a1f5147eb3467a65b693f4c8c1443f5b5d
TTSpaxxy <throughthefire@derpymail.org>
2014-11-08 11:48:23 -0800

    Initial Commit

---
+++ desktop/src/com/cal/codeday/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cal.codeday.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cal.codeday.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
