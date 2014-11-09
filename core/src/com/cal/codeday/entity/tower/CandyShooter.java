1f9e5db6c5a92cfad6338f50bed2115f1d886dfb
TTSpaxxy <throughthefire@derpymail.org>
2014-11-09 05:59:32 -0800

    WE CAN DO THIS GUYS

--- core/src/com/cal/codeday/entity/tower/CandyShooter.java
+++ core/src/com/cal/codeday/entity/tower/CandyShooter.java
@@ -7,10 +7,12 @@ import com.cal.codeday.level.Level;
  */
 public class CandyShooter extends Tower {
 
+    String projectileTexture = "gfx/spriteFinal/Towers/PresentShooter/projectileCandyYellow01.png";
+
     public CandyShooter(Level currentLevel) {
-        super("placeholder2", 64, currentLevel);
-        range = 60f;
-        fire_rate = 3f;
+        super("gfx/spriteFinal/Towers/CandyShooter/towerCandyShooter", 64, currentLevel);
+        range = 20f;
+        fire_rate = 12f;
         cost = 100;
         power = 5f;
     }
